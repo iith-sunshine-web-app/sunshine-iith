69d2e36ff1cb46b03fde71819122bab914104417
Vivek <365anu65@gmail.com>
2024-10-06 16:40:45 +0530

    Heads seperate section

--- app/buddies/buddies.js
+++ app/buddies/buddies.js
@@ -1,6 +1,15 @@
 import teamMembers from "../non_management";
 const teams = [
   {
+    id: "ug-buddy-heads",
+    section: "UG Buddy Heads",
+    teamName: "UG Buddy Heads",
+    teamPhotoUrl: "https://sunshine.iith.ac.in/images/about-slider/3.JPG",
+    teamDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui expedita perferendis unde debitis sunt reprehenderit vero asperiores tempore, officia excepturi fugit aliquam minus voluptas. At aut, voluptatum magnam, praesentium voluptatibus in explicabo vero illum nesciunt nostrum molestias saepe? Commodi, nihil. Magni ipsam, eaque impedit fuga nobis enim rem sapiente accusantium?",
+    teamMembers: teamMembers["UG Buddy Heads"],
+  },
+  {
     id: "ug-buddies",
     section: "UG Buddies",
     teamName: "UG Buddies",
@@ -8,6 +17,15 @@ const teams = [
     teamDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui expedita perferendis unde debitis sunt reprehenderit vero asperiores tempore, officia excepturi fugit aliquam minus voluptas. At aut, voluptatum magnam, praesentium voluptatibus in explicabo vero illum nesciunt nostrum molestias saepe? Commodi, nihil. Magni ipsam, eaque impedit fuga nobis enim rem sapiente accusantium?",
     teamMembers: teamMembers["UG Buddy"],
+  },
+  {
+    id: "pg-buddy-heads",
+    section: "PG Buddy Heads",
+    teamName: "PG Buddy Heads",
+    teamPhotoUrl: "https://sunshine.iith.ac.in/images/about-slider/3.JPG",
+    teamDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui expedita perferendis unde debitis sunt reprehenderit vero asperiores tempore, officia excepturi fugit aliquam minus voluptas. At aut, voluptatum magnam, praesentium voluptatibus in explicabo vero illum nesciunt nostrum molestias saepe? Commodi, nihil. Magni ipsam, eaque impedit fuga nobis enim rem sapiente accusantium?",
+    teamMembers: teamMembers["PG Buddy Heads"],
   },
   {
     id: "pg-buddies",
