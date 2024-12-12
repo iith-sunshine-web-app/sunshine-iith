84e3508498fc94afe6cb0b253be1ef64bb14ebb1
iith-sunshine-web-app <112944645+iith-sunshine-web-app@users.noreply.github.com>
2024-12-12 18:18:23 +0530

    Update facultycoordinators.js, Corrected the FIC department

--- app/teams/facultycoordinators.js
+++ app/teams/facultycoordinators.js
@@ -2,7 +2,7 @@ const facultyCoordinators = [
   {
     name: "Dr. Neeraj Kumar",
     email: "fic.sunshine@iith.ac.in",
-    department: "Liberal Arts | Engineering Science",
+    department: "Liberal Arts",
     image:
       "/../../FacultyImages/Neeraj_Kumar.png",
   },
